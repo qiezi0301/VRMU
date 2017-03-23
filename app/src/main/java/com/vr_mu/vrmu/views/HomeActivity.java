988d2f585c6035e70a7e5eb4048b82c43ac3d8cf
qiezi0301 <qiezi0301@163.com>
2017-03-23 20:14:41 +0800

    添加碎片布局文件

--- app/src/main/java/com/vr_mu/vrmu/views/HomeActivity.java
+++ app/src/main/java/com/vr_mu/vrmu/views/HomeActivity.java
@@ -3,27 +3,44 @@ package com.vr_mu.vrmu.views;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TextView;
 
 import com.vr_mu.vrmu.R;
+import com.vr_mu.vrmu.views.fragments.FindHomeFragment;
 import com.vr_mu.vrmu.views.fragments.HomeFragment;
+import com.vr_mu.vrmu.views.fragments.LiveHomeFragment;
+import com.vr_mu.vrmu.views.fragments.ServerHomeFragment;
+import com.vr_mu.vrmu.views.fragments.SongHomeFragment;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends FragmentActivity {
 
 
     private static final String TAG = "HomeActivity";
     private DrawerLayout mDrawerLayout;
 
+    /**
+     * Fragment数组界面
+     */
+    private final Class mFragmentArray[] = {LiveHomeFragment.class, SongHomeFragment.class, HomeFragment.class, ServerHomeFragment.class, FindHomeFragment.class};
+    private int mImageArray[] = {R.drawable.live_white_default, R.drawable.music_white, R.drawable.service_white, R.drawable.service_white, R.drawable.find_white};
+    private String mTextArray[] = {"哇-Live", "音乐", "首页", "服务", "发现"};
+
+    private LayoutInflater mLayoutInflater;
+    private FragmentTabHost mTabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+        initView();
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 
@@ -45,15 +62,43 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.content_lay, new HomeFragment());
-        transaction.commit();
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction transaction = fragmentManager.beginTransaction();
+//        transaction.replace(R.id.content_lay, new HomeFragment());
+//        transaction.commit();
 
     }
 
+    private void initView() {
+        mLayoutInflater = LayoutInflater.from(this);
+        // 找到TabHost
+        mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.content_lay);
 
+        // 得到fragment的个数
+        int fragmentCount = mFragmentArray.length;
+        for (int i = 0; i < fragmentCount; i++) {
+            // 给每个Tab按钮设置图标、文字和内容
+            TabHost.TabSpec tabSpec = mTabHost.newTabSpec(mTextArray[i]).setIndicator(getTabItemView(i));
+            // 将Tab按钮添加进Tab选项卡中
+            mTabHost.addTab(tabSpec, mFragmentArray[i], null);
+            // 设置Tab按钮的背景
+            mTabHost.getTabWidget().setDividerDrawable(null);
+        }
+    }
 
+    /**
+     * 给每个Tab按钮设置图标和文字
+     */
+    private View getTabItemView(int index) {
+        View view = mLayoutInflater.inflate(R.layout.tab_item_view, null);
+        ImageView imageView = (ImageView) view.findViewById(R.id.tab_icon);
+        imageView.setImageResource(mImageArray[index]);
+        TextView textView = (TextView) view.findViewById(R.id.tab_tv);
+        textView.setText(mTextArray[index]);
+
+        return view;
+    }
 
 
 //    @Override
